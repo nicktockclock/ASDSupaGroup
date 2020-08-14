089ed7b6763fb44ab45e9fc0133f7aa0b9569567
tiffylee <46877769+tiffylee@users.noreply.github.com>
2020-08-14 16:45:21 +1000

    signup page added

---
+++ recipewebapp/src/Routes.js
@@ -0,0 +1,19 @@
+import Signup from "./containers/Signup";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+    </Switch>
+  );}
+
+
+
--- recipewebapp/src/index.js
+++ recipewebapp/src/index.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,11 +6,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
