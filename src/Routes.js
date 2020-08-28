635b854332f1f9830c3c69fe592285754ff681ca
fbrennanbohm <felix.brennanbohm@gmail.com>
2020-08-28 14:58:53 +1000

    folder restructure

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import Signup from "./containers/Signup";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+    </Switch>
+  );}
+
+
+
---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Welcome to Recipe-zee-HDeasy</h1>
+      </div>
+    </div>
+  );
+}
