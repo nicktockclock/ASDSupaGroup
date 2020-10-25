6c60a7011bb27aa9a7790a30027e12f48bc4a80b
fbrennanbohm <felix.brennanbohm@gmail.com>
2020-10-25 16:57:39 +1100

    - Hid sign up button for those already logged in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { LinkContainer } from "react-router-bootstrap";
 import { AppContext } from "./libs/contextLib";
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Routes from "./Routes";
 import { Route, Switch, useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -8,12 +8,12 @@ import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import "./containers/RecipePage.js";
 import UpdateRecipe from "./containers/UpdateRecipe";
-import {AuthContext} from './libs/AuthContext';
+import { AuthContext } from './libs/AuthContext';
 
 function App() {
-  const {isAuthorised, logout, email} = useContext(AuthContext);
+  const { isAuthorised, logout, email } = useContext(AuthContext);
   const history = useHistory();
-  
+
 
   return (
     <div className="App container">
@@ -26,53 +26,55 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
+            {!isAuthorised &&
+              <LinkContainer to="/signup">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+            }
             <LinkContainer to="/recipepage">
               <NavItem>Recipe Page</NavItem>
             </LinkContainer>
             {isAuthorised &&
-            <LinkContainer 
-              to={{
+              <LinkContainer
+                to={{
                   pathname: "/createrecipe",
-                  state: {email: email}
-              }}
-            >
-              <NavItem>Create Recipe</NavItem>
-            </LinkContainer>
+                  state: { email: email }
+                }}
+              >
+                <NavItem>Create Recipe</NavItem>
+              </LinkContainer>
             }
             <LinkContainer to="/listrecipes">
               <NavItem>Browse</NavItem>
             </LinkContainer>
             {isAuthorised &&
-            <LinkContainer 
-              to={{
+              <LinkContainer
+                to={{
                   pathname: "/myrecipes",
-                  state: {email: email}
-              }}
-            >
-              <NavItem>My Recipes</NavItem>
-            </LinkContainer>
+                  state: { email: email }
+                }}
+              >
+                <NavItem>My Recipes</NavItem>
+              </LinkContainer>
             }
-            {isAuthorised && 
+            {isAuthorised &&
               <LinkContainer to="/accountmanagement">
-              <NavItem>Account Management</NavItem>
-            </LinkContainer>
+                <NavItem>Account Management</NavItem>
+              </LinkContainer>
             }
             {!isAuthorised ? (
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+              <LinkContainer to="/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
             ) : (
-              <NavItem onClick={() => {logout(); history.push('/login');}}>Logout</NavItem>
-            )}
+                <NavItem onClick={() => { logout(); history.push('/login'); }}>Logout</NavItem>
+              )}
             {email && <NavItem disabled>{email}</NavItem>}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-  <Routes>
-  </Routes>
+      <Routes>
+      </Routes>
     </div>
   );
 }
