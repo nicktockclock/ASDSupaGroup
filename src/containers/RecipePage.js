08b85af51797ae8c424feaa55ae2b24eba9f3f59
harryoui <harryouil@hotmail.com>
2020-10-25 18:16:23 +1100

    rewrote RecipePage to be dynamic

--- src/containers/RecipePage.js
+++ src/containers/RecipePage.js
@@ -1,53 +1,78 @@
 import React, { Component, useState } from 'react';
 import { Button, Form, FormGroup, FormControl, Col, Checkbox, ControlLabel } from 'react-bootstrap';
 import Routes from "../Routes";
+import queryString from 'query-string';
+import Axios from 'axios';
+import {getSorted, getRecipeMetadata} from "../components/RecipeDisplay";
 
 class RecipePage extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Crispy Fried Chicken </h1>
-    
-             <input type="button" value="Save"></input>
-             <button><a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank">
-                Share on Facebook
-              </a>
-            </button>
 
+  constructor(props) {
+    super(props);
+    this.state = { recipe : "" };
+  }
 
-     <br></br>
+  async componentDidMount() {
+    const values = queryString.parse(this.props.location.search);
+    console.log(values.test);
+    var requestedId = values.id;
 
-    <img src="Fried Chicken.jpg" alt="Fried Chicken" width="500" height="600"></img>
-    
- <h3>Step 1</h3>
-    
-     <p>
-       In a large shallow dish, combine 2-2/3 cups flour, 2 tablespoons garlic salt, 1 tablespoon paprika, 2-1/2 teaspoons pepper and 2-1/2 teaspoons poultry seasoning. 
-     </p>
-    
-     <h3>Step 2</h3>
-    
-     <p>In another shallow dish, beat eggs and 1-1/2 cups water; add 1 teaspoon salt and the remaining 1-1/3 cup flour and 1/2 teaspoon pepper. Dip chicken in egg mixture, then place in the flour mixture, a few pieces at a time. Turn to coat.</p>
-    
-     <h3>Step 3</h3>
-    
-     <p>Dip chicken in egg mixture, then place in the flour mixture, a few pieces at a time. Turn to coat.</p>
-
-     <h3>Step 4</h3>
-    
-     <p>In a deep-fat fryer, heat oil to 375°. Working in batches, fry chicken, several pieces at a time, until golden brown and a thermometer inserted into chicken reads 165°, about 7-8 minutes on each side. Drain on paper towels.</p>
-
-     <h3>Step 5</h3>
-    
-     <p>Working in batches, fry chicken, several pieces at a time, until golden brown and a thermometer inserted into chicken reads 165°, about 7-8 minutes on each side. Drain on paper towels.</p>
-
-     <h3>Step 6</h3>
-    
-    <p>Drain on paper towels.</p>
-    hello
-            </div>
-        )
+    var res = "";
+    if (requestedId) {
+      var query = 'http://localhost:5000/api/Recipes/' + requestedId;
+      await Axios.get(query).then(r => res = r.data);
+    } else {
+      //get random recipe
+      res = await getSorted({sort:"random"}).then(r => res = r[0]);
     }
-}
-export default RecipePage;
 
+    if (!res) return; //dont continue on no return
+    var r = await getRecipeMetadata(res)
+
+    //edit empty values
+    if (!r.owner) r.owner = "Anonymous";
+
+    this.setState( {recipe : r} );
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <center>
+          <h1> {this.state.recipe.recipeName} </h1> 
+          <h4>by {this.state.recipe.owner}</h4>
+          
+          <input type="button" value="Save"></input>
+          <button>
+            <a href={
+              "https://www.facebook.com/sharer/sharer.php?u=" + 
+              window.location.href.replace("localhost", "127.0.0.1")} //facebook won't accept localhost
+              target="_blank"
+            >
+              Share on Facebook
+            </a>
+          </button>
+
+          <br></br><br></br>
+
+          <img src={this.state.recipe.url} alt="Fried Chicken" width="100%"/>
+          <h2>{this.state.recipe.description}</h2>
+        </center>
+
+        <br></br>
+
+        <p>{this.state.recipe.instructions}</p>
+
+        <br></br>
+
+        <div>
+          <p>Ingredients: {this.state.recipe.ingredients}</p>
+          <p>Cooking Time: {this.state.recipe.cookTime}</p>
+          <p>Difficulty: {this.state.recipe.difficulty}</p>
+          <p>Serves: {this.state.recipe.servings}</p>
+          <p>Calories: {this.state.recipe.calories}</p>
+        </div>
+      </div>
+    )
+  }
+} export default RecipePage
