cd979e911c0e795e97831b3e5ba3498d0db28bb0
fbrennanbohm <felix.brennanbohm@gmail.com>
2020-10-25 18:58:45 +1100

    Added search bar (not working)

--- src/containers/AccountManager.js
+++ src/containers/AccountManager.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Table, Alert } from 'react-bootstrap';
+import { Button, Table, Alert, Form, Col, FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
 import './AccountManager.css';
 import data from '../data/users.json'
 import { Link } from 'react-router-dom';
@@ -18,12 +18,12 @@ class AccountManager extends Component {
     }
 
     getUsers = async () => {
-        this.setState({loading: true, error: null})
+        this.setState({ loading: true, error: null })
         try {
             const response = await api().get('/users');
-            this.setState({users: response.data, loading: false});
-        } catch(e) {
-            this.setState({error: e, loading: false});
+            this.setState({ users: response.data, loading: false });
+        } catch (e) {
+            this.setState({ error: e, loading: false });
         }
     }
 
@@ -32,11 +32,11 @@ class AccountManager extends Component {
     }
 
     removeUser = async (id) => {
-        this.setState({loading: true, error: null})
+        this.setState({ loading: true, error: null })
         try {
             await api().delete(`/users/${id}`);
-        } catch(e) {
-            this.setState({error: e, loading: false});
+        } catch (e) {
+            this.setState({ error: e, loading: false });
         }
         await this.getUsers();
     }
@@ -47,36 +47,48 @@ class AccountManager extends Component {
         const { users, error, loading } = this.state
         return (
             <>
-            {error && <Alert error><pre>{JSON.stringify(error, null, 2)}</pre></Alert>}
-            <Table>
-                <thead>
-                    <tr>
-                        <th>Email</th>
-                        <th className='button-header'>Edit</th>
-                        <th className='button-header'>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    loading ? (
-                        <tr><td>Loading...</td></tr>
-                    ) : (
-                        users.map((user, index) => {
-                            return (
-                                    <tr key={index}>
-                                        <td>{user.email}</td>
-                                        <td>
-                                            <Link to={{ pathname: `/accountmanagement/updateuser/${user.userID}`, }} userName={user.userID}>Update</Link></td>
-                                        <td align='left'><Button type="button" variant="danger" onClick={(e) => {e.preventDefault(); this.removeUser(user.userID)}}>Delete</Button></td>
-                                        {/* <td align='left'><Button variant="danger" onClick={() => this.removeUser(user.id)}>Delete</Button></td> */}
-                                    </tr>
-                            )
-                        })
-                    )
-                }
+                {error && <Alert error><pre>{JSON.stringify(error, null, 2)}</pre></Alert>}
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>
+                                <Form horizontal>
+                                    <FormGroup controlId="formHorizontalName" bsSize='large'>
+                                        <Col componentClass={ControlLabel} sm={2}>User email</Col>
+                                        <Col sm={10}>
+                                            <FormControl type="text" placeholder={this.state.name} value={this.state.name} onChange={this.onChangeRecipeName} />
+                                        </Col>
+                                    </FormGroup>
+                                </Form>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <th className='button-header'>Edit</th>
+                            <th className='button-header'>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            loading ? (
+                                <tr><td>Loading...</td></tr>
+                            ) : (
+                                    users.map((user, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{user.email}</td>
+                                                <td>
+                                                    <Link to={{ pathname: `/accountmanagement/updateuser/${user.userID}`, }} userName={user.userID}>Update</Link></td>
+                                                <td align='left'><Button type="button" variant="danger" onClick={(e) => { e.preventDefault(); this.removeUser(user.userID) }}>Delete</Button></td>
+                                                {/* <td align='left'><Button variant="danger" onClick={() => this.removeUser(user.id)}>Delete</Button></td> */}
+                                            </tr>
+                                        )
+                                    })
+                                )
+                        }
 
-</tbody>
-            </Table>
+                    </tbody>
+                </Table>
             </>
         )
     }
